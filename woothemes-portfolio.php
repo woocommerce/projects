67e4133b6d232295cdb16254b3ff6c5b131f2bf9
Matty <hello@matty.co.za>
2013-07-03 12:58:54 +0200

    Adds version number tracking to the main plugin file.

--- woothemes-portfolio.php
+++ woothemes-portfolio.php
@@ -2,13 +2,13 @@
 /**
  * Plugin Name: Portfolio
  * Plugin URI: http://woothemes.com/
- * Description: Hi, I'm your portfolio showcase plugin for WordPress. Show off your recent work, using our shortcode, widget or template tag.
+ * Description: Hi, I'm your portfolio showcase plugin for WordPress. Show off your recent work using our shortcode, widget or template tag.
  * Author: WooThemes
  * Version: 1.0.0
  * Author URI: http://woothemes.com/
  *
  * @package WordPress
- * @subpackage Woothemes_portfolios
+ * @subpackage Woothemes_Portfolio
  * @author Matty
  * @since 1.0.0
  */
@@ -18,3 +18,4 @@ require_once( 'woothemes-portfolio-template.php' );
 require_once( 'classes/class-woothemes-widget-portfolio.php' );
 global $woothemes_portfolio;
 $woothemes_portfolio = new Woothemes_Portfolio( __FILE__ );
+$woothemes_portfolio->version = '1.0.0';

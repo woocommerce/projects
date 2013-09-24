e224f4116d9cde4202ed93a32dbdce027e7d22e5
James Koster <james@jameskoster.co.uk>
2013-09-24 12:07:42 +0100

    no add to cart :-)

--- woothemes-projects-hooks.php
+++ woothemes-projects-hooks.php
@@ -44,12 +44,8 @@ if ( ! is_admin() || defined('DOING_AJAX') ) {
 	/**
 	 * Project Loop Items
 	 *
-	 * @see woothemes_projects_template_loop_add_to_cart()
 	 * @see woothemes_projects_template_loop_project_thumbnail()
-	 * @see woothemes_projects_template_loop_price()
-	 * @see woothemes_projects_template_loop_rating()
 	 */
-	add_action( 'woothemes_projects_after_showcase_loop_item', 'woothemes_projects_template_loop_add_to_cart', 10 );
 	add_action( 'woothemes_projects_before_showcase_loop_item_title', 'woothemes_projects_template_loop_project_thumbnail', 10 );
 
 	/**

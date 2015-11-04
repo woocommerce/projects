bb24003169f8155cb2a28c790caef4feb3c0fc4f
Jon Ang <jon@wingzcommunications.com>
2015-11-04 13:13:47 +0800

    Update class-projects-admin.php
    Fixes #109
    
    Checks for post-new.php as well when enqueuing the admin.js which fixes the buttons not working when the post type has not been saved at least once.
    
    This should fix all upload and gallery buttons
    
    Also put the wp_localize_script line into the if-then-else, since there is no point localising if the script isn't enqueued.

--- classes/class-projects-admin.php
+++ classes/class-projects-admin.php
@@ -570,19 +570,21 @@ class Projects_Admin {
 
 		wp_enqueue_style( 'projects-admin', $this->assets_url . '/css/admin.css', array(), '1.0.0' );
 
-		if ( $pagenow == 'post.php' && get_post_type() == $this->post_type ) {
+		if ( ( $pagenow == 'post.php' || $pagenow == 'post-new.php' ) && get_post_type() == $this->post_type ) {
 			wp_enqueue_script( 'projects-admin', $this->assets_url . '/js/admin.js', array( 'jquery' ), '1.0.0', true );
-		}
+		
 
-		wp_localize_script( 'projects-admin', 'woo_projects_admin',
-				array(
-					'gallery_title' 	=> __( 'Add Images to Project Gallery', 'projects-by-woothemes' ),
-					'gallery_button' 	=> __( 'Add to gallery', 'projects-by-woothemes' ),
-					'delete_image'		=> __( 'Delete image', 'projects-by-woothemes' ),
-					'default_title' 	=> __( 'Upload', 'projects-by-woothemes' ),
-					'default_button' 	=> __( 'Select this', 'projects-by-woothemes' ),
-				)
-			);
+			wp_localize_script( 'projects-admin', 'woo_projects_admin',
+					array(
+						'gallery_title' 	=> __( 'Add Images to Project Gallery', 'projects-by-woothemes' ),
+						'gallery_button' 	=> __( 'Add to gallery', 'projects-by-woothemes' ),
+						'delete_image'		=> __( 'Delete image', 'projects-by-woothemes' ),
+						'default_title' 	=> __( 'Upload', 'projects-by-woothemes' ),
+						'default_button' 	=> __( 'Select this', 'projects-by-woothemes' ),
+					)
+				);
+			
+		}
 
 	} // End enqueue_admin_styles()
 
